1766c5033b9e5bcdec568fcf95c973f8bf862516
Charlie Tsai <Charlie_Tsai@asus.com>
2024-07-30 10:59:23 +0800

    init
    Signed-off-by: Charlie Tsai <Charlie_Tsai@asus.com>

---
+++ TestForSSL/Controllers/OracleTestController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Mvc;
+using Oracle.ManagedDataAccess.Client;
+
+namespace TestForSSL.Controllers
+{
+	public class OracleTestController : Controller
+	{
+		public ActionResult TestConnection()
+		{
+			string connectionString = ConfigurationManager.ConnectionStrings["OracleDbContext"].ConnectionString;
+			string connectionStringSSL = ConfigurationManager.ConnectionStrings["OracleDbContextSSL"].ConnectionString;
+			string result = "";
+
+			try
+			{
+				using (OracleConnection connection = new OracleConnection(connectionString))
+				{
+					connection.Open();
+					result += "Successfully connected to Oracle Database!<br/>";
+
+					using (OracleCommand command = connection.CreateCommand())
+					{
+						command.CommandText = "SELECT SYSDATE FROM DUAL";
+						object dateResult = command.ExecuteScalar();
+						result += $"Current date from Oracle: {dateResult}";
+						result += "<br/>";
+					}
+				}
+
+				using (OracleConnection connection = new OracleConnection(connectionStringSSL))
+				{
+					connection.Open();
+					result += "Successfully connected to Oracle Database with SSL!<br/>";
+
+					using (OracleCommand command = connection.CreateCommand())
+					{
+						command.CommandText = "SELECT SYSDATE FROM DUAL";
+						object dateResult = command.ExecuteScalar();
+						result += $"Current date from Oracle: {dateResult}";
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				result += $"Error: {ex.Message}";
+			}
+
+			ViewBag.Result = result;
+			return View();
+		}
+	}
+}
---
+++ TestForSSL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestForSSL.MvcApplication" Language="C#" %>
